c6d4f45a9b6ee12d0e6a349602882d106891c747
VINEETH DASARI <dasariwineethoo7@gmail.com>
2024-05-23 00:48:51 +0530

    create and verify JWT web tokens are done in service layer

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,22 +1,43 @@
-const UserRepository = require('../repository/user-repository');
+const UserRepository = require("../repository/user-repository");
+const jwt = require('jsonwebtoken');
+const {JWT_KEY} =require('../config/serverConfig');
 
 class UserService {
-    constructor() {
-        this.userRepository = new UserRepository();
-    }
+  constructor() {
+    this.userRepository = new UserRepository();
+  }
 
-    async create(data) {
-        try {
-            //console.log(data);
-            const user = await this.userRepository.create(data);
-            return user;
-        } catch (error) {
-            console.log("Something went wrong in the service layer");
-            throw error;
-        }
+  async create(data) {
+    try {
+      //console.log(data);
+      const user = await this.userRepository.create(data);
+      return user;
+    } catch (error) {
+      console.log("Something went wrong in the service layer");
+      throw error;
     }
+  }
+
+  createToken(user) {
+    try {
+      const result = jwt.sign(user, JWT_KEY, { expiresIn: "1d" });
+      return result;
+    } catch (error) {
+      console.log("Something went wrong in token creation");
+      throw error;
+    }
+  }
+
+  verifyToken(token) {
+    try {
+      const response = jwt.verify(token, JWT_KEY);
+      return response;
+    } catch (error) {
+      console.log("Something went wrong in token validation", error);
+      throw error;
+    }
+  }
 
 }
-
 
 module.exports = UserService;
